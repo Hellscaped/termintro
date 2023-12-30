0cbd415453fce271a9ac0ff7ae580b953c8b7ef4
Hellscaped <tappsyo@gmail.com>
2023-12-30 02:26:53 -0600

    removed a line

--- intro.py
+++ intro.py
@@ -2,8 +2,7 @@ import os, PIL.Image, time
 intro = PIL.Image.open("intro.gif")
 for i in range(intro.n_frames):
     intro.seek(i)
-    frame = intro.convert("RGB")
-    frame = frame.resize((os.get_terminal_size().columns, os.get_terminal_size().lines-1), PIL.Image.NEAREST)
+    frame = intro.convert("RGB").resize((os.get_terminal_size().columns, os.get_terminal_size().lines-1), PIL.Image.NEAREST)
     os.system("clear")
     for y in range(os.get_terminal_size().lines-1):
         line = ""
